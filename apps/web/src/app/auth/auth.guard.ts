78d1cc8269974152082c66164ca993f369400734
Andry Gunawan <angun33@gmail.com>
2021-06-07 13:34:19 +1000

    Auth guard now try to refresh the token first before redirecting to the login page

--- apps/web/src/app/auth/auth.guard.ts
+++ apps/web/src/app/auth/auth.guard.ts
@@ -1,27 +1,33 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable, Subject} from "rxjs";
 import {AuthService} from "./auth.service";
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private auth:AuthService, private router:Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['login', {'redirectTo': state.url}]);
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    return this._isAuthenticated(state);
+  }
 
-      return false;
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    return this._isAuthenticated(state);
+  }
+
+  private _isAuthenticated(state: RouterStateSnapshot) : Observable<boolean> | boolean {
+    if (!this.auth.isAuthenticated()) {
+      const subject = new Subject<boolean>();
+      this.auth.refresh()
+        .subscribe(
+          () => subject.next(true),
+          () => {
+            this.router.navigate(['login', {'redirectTo': state.url}]);
+            subject.next(false);
+          }
+        )
+      return subject.asObservable();
     }
     return true;
   }
-
-  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['login', {'redirectTo': state.url}]);
-
-      return false;
-    }
-    return true;
-  }
-
 }
